992e02078834b9487c8d86f021b02109e7c1cd2a
abicelis <abicelis@gmail.com>
2018-05-25 22:11:15 -0400

    Added std constants and message classes

---
+++ app/src/main/java/ve/com/abicelis/cryptomaster/application/Constants.java
@@ -0,0 +1,56 @@
+package ve.com.abicelis.cryptomaster.application;
+
+/**
+ * Created by abicelis on 24/5/2018.
+ * App-wide constants file
+ * Element	                    Field Name Prefix
+ * SharedPreferences	        PREF_
+ * Bundle	                    BUNDLE_
+ * Fragment Arguments	        ARGUMENT_
+ * Intent Extra	                EXTRA_
+ * Intent Action	            ACTION_
+ * Start Activity for result    RESULT_
+ * Constant                     CONST_
+ * Miscellaneous                MISC_
+ */
+
+public class Constants {
+
+    /* MISC */
+    public static final int    MISC_IMAGE_JPEG_COMPRESSION_PERCENTAGE = 50;
+    public static final int    MISC_ALPHA_ANIMATIONS_DURATION = 200;
+    public static final int    MISC_UI_DEBOUNCE_TIME_MILLISECONDS = 300;
+
+
+    /* HOME ACTIVITY */
+
+    /* FLIGHT ACTIVITY */
+//    public static final String  EXTRA_ACTIVITY_FLIGHT_TRIP_ID = "EXTRA_ACTIVITY_FLIGHT_TRIP_ID";
+//    public static final String  EXTRA_ACTIVITY_FLIGHT_FLIGHT_ID = "EXTRA_ACTIVITY_FLIGHT_FLIGHT_ID";
+//    public static final String  EXTRA_ACTIVITY_FLIGHT_FLIGHT_POSITION = "EXTRA_ACTIVITY_FLIGHT_FLIGHT_POSITION";
+//    public static final String  TAG_ACTIVITY_FLIGHT_AIRPORT_AIRLINE_SEARCH_FRAGMENT = "TAG_ACTIVITY_FLIGHT_AIRPORT_AIRLINE_SEARCH_FRAGMENT";
+//    public static final String  TAG_ACTIVITY_FLIGHT_DATE_SELECT_FRAGMENT = "TAG_ACTIVITY_FLIGHT_DATE_SELECT_FRAGMENT";
+//    public static final String  TAG_ACTIVITY_FLIGHT_NUMBER_SELECT_FRAGMENT = "TAG_ACTIVITY_FLIGHT_NUMBER_SELECT_FRAGMENT";
+//    public static final String  TAG_ACTIVITY_FLIGHT_FLIGHT_RESULTS_FRAGMENT = "TAG_ACTIVITY_FLIGHT_FLIGHT_RESULTS_FRAGMENT";
+
+
+
+    /* SHARED PREFERENCES */
+    public static final String  SHARED_PREFERENCE_TIME_FORMAT = "SHARED_PREFERENCE_TIME_FORMAT";
+    public static final String  SHARED_PREFERENCE_DATE_FORMAT = "SHARED_PREFERENCE_DATE_FORMAT";
+    public static final String  SHARED_PREFERENCE_APP_THEME_TYPE = "SHARED_PREFERENCE_APP_THEME_TYPE";
+
+
+    /* ROOM DATABASE */
+    public static final String ROOM_DATABASE_NAME = "crypto_master.db";
+    public static final String ROOM_DATABASE_CALENDAR_CONVERTER_SEPARATOR = "!!!";
+    public static final int    ROOM_DATABASE_MAX_SEARCH_RESULTS = 10;
+
+
+    /* COINMARKETCAP API */
+    public static final String COINMARKETCAP_BASE_URL = "https://api.coinmarketcap.com/v2/";
+
+    /* CRYPTOCOMPARE API */
+    public static final String CRYPTOCOMPARE_BASE_URL = "https://api.qwant.com/api/search/";
+
+}
---
+++ app/src/main/java/ve/com/abicelis/cryptomaster/application/Message.java
@@ -0,0 +1,44 @@
+package ve.com.abicelis.cryptomaster.application;
+
+import android.content.Context;
+import android.support.annotation.StringRes;
+
+import ve.com.abicelis.cryptomaster.R;
+import ve.com.abicelis.cryptomaster.util.SnackbarUtil;
+
+/**
+ * Created by abicelis on 25/5/2018.
+ * App-wide Messages
+ * ERROR_
+ * NOTICE_
+ * SUCCESS_
+ *
+ */
+
+public enum Message {
+    ERROR_UNEXPECTED(R.string.error_unexpected, SnackbarUtil.SnackbarType.ERROR),
+
+    ;
+
+    @StringRes
+    int friendlyName;
+    SnackbarUtil.SnackbarType messageType;
+
+    Message(@StringRes int friendlyName, SnackbarUtil.SnackbarType messageType) {
+        this.friendlyName = friendlyName;
+        this.messageType = messageType;
+    }
+
+
+    public @StringRes
+    int getFriendlyNameRes() {
+        return friendlyName;
+    }
+    public SnackbarUtil.SnackbarType getMessageType() {
+        return messageType;
+    }
+    public String getFriendlyName(Context context) {
+        return CryptoMasterApplication.getAppContext().getString(friendlyName);
+    }
+
+}
