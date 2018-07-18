2168ad42a826331b63884b89f324225a341b1b97
abicelis <abicelis@gmail.com>
2018-07-18 14:24:41 -0400

    Sorted alphabetically by friendly name, added missing symbols

--- app/src/main/java/ve/com/abicelis/cryptomaster/data/model/Currency.java
+++ app/src/main/java/ve/com/abicelis/cryptomaster/data/model/Currency.java
@@ -3,61 +3,61 @@ package ve.com.abicelis.cryptomaster.data.model;
 /**
  * Created by abicelis on 31/5/2018.
  */
-public enum Currency {
-    AUD("Australia Dollar",         "$",        "AUD"),
-    BRL("Brazil Real",              "R$",       "BRL"),
-    CAD("Canada Dollar",            "$",        "CAD"),
-    CHF("Swiss Franc",              "CHF",      "CHF"),
-    CLP("Chile Peso",               "$",        "CLP"),
-    CNY("China Yuan",               "¥",        "CNY"),
-    CZK("Czech Koruna",             "Kč",       "CZK"),
-    DKK("Denmark Krone",            "kr",       "DKK"),
-    EUR("Euro",                     "€",        "EUR"),
-    GBP("United Kingdom Pound",     "£",        "GBP"),
-    HKD("Hong Kong Dollar",         "$",        "HKD"),
-    HUF("Hungary Forint",           "Ft",       "HUF"),
-    IDR("Indonesia Rupiah",         "Rp",       "IDR"),
-    ILS("Israel Shekel",            "₪",        "ILS"),
-    INR("India Rupee",              "INR",      "INR"),
-    JPY("Japan Yen",                "¥",        "JPY"),
-    KRW("Korea Won",                "₩",        "KRW"),
-    MXN("Mexico Peso",              "$",        "MXN"),
-    MYR("Malaysia Ringgit",         "RM",       "MYR"),
-    NOK("Norway Krone",             "kr",       "NOK"),
-    NZD("New Zealand Dollar",       "$",        "NZD"),
-    PHP("Philippines Piso",         "₱",        "PHP"),
-    PKR("Pakistan Rupee",           "₨",        "PKR"),
-    PLN("Poland Zloty",             "zł",       "PLN"),
-    RUB("Russia Ruble",             "\u20BD",   "RUB"),
-    SEK("Sweden Krona",             "kr",       "SEK"),
-    SGD("Singapore Dollar",         "$",        "SGD"),
-    THB("Thai Baht",                "฿",        "THB"),
-    TRY("Turkey lira",              "TRY",      "TRY"),
-    TWD("Taiwan New Dollar",        "$",        "TWD"),
-    USD("United States Dollar",     "$",        "USD"),
-    ZAR("South Africa Rand",        "ZAR",      "ZAR"),
-    BTC("Bitcoin",                  "฿",        "BTC"),
-    ETH("Ethereum",                 "",         "ETH"),
-    XRP("Ripple",                   "",         "XRP"),
-    LTC("Litecoin",                 "",         "LTC"),
-    BCH("Bitcoin Cash",             "",         "BCH")
+public enum Currency {    USD("United States Dollar",     "$",        true,      "USD"),
+    AUD("Australian Dollar",        "$",        true,      "AUD"),
+    BRL("Brazilian Real",           "R$",       true,      "BRL"),
+    CAD("Canadian Dollar",          "$",        true,      "CAD"),
+    CLP("Chilean Peso",             "$",        true,      "CLP"),
+    CNY("Chinese Yuan",             "¥",        true,      "CNY"),
+    CZK("Czech Koruna",             "Kč",       true,      "CZK"),
+    DKK("Danish Krone",             "kr",       true,      "DKK"),
+    EUR("Euro",                     "€",        true,      "EUR"),
+    HKD("Hong Kong Dollar",         "$",        true,      "HKD"),
+    HUF("Hungarian Forint",         "Ft",       true,      "HUF"),
+    INR("Indian Rupee",             "₹",        true,      "INR"),
+    IDR("Indonesian Rupiah",        "Rp",       true,      "IDR"),
+    ILS("Israeli Shekel",           "₪",        true,      "ILS"),
+    JPY("Japanese Yen",             "¥",        true,      "JPY"),
+    KRW("Korean Won",               "₩",        true,      "KRW"),
+    MYR("Malaysian Ringgit",        "RM",       true,      "MYR"),
+    MXN("Mexican Peso",             "$",        true,      "MXN"),
+    NZD("New Zealand Dollar",       "$",        true,      "NZD"),
+    NOK("Norwegian Krone",          "kr",       true,      "NOK"),
+    PKR("Pakistani Rupee",          "₨",        true,      "PKR"),
+    PHP("Philippine Piso",          "₱",        true,      "PHP"),
+    PLN("Polish Zloty",             "zł",       true,      "PLN"),
+    RUB("Russian Ruble",            "\u20BD",   true,      "RUB"),
+    SGD("Singaporian Dollar",       "$",        true,      "SGD"),
+    ZAR("South African Rand",       "R",        true,      "ZAR"),
+    SEK("Swedish Krona",            "kr",       true,      "SEK"),
+    CHF("Swiss Franc",              "",         false,     "CHF"),
+    TWD("Taiwanese New Dollar",     "$",        true,      "TWD"),
+    THB("Thai Baht",                "฿",        true,      "THB"),
+    TRY("Turkish lira",             "₺",        true,      "TRY"),
+    GBP("United Kingdom Pound",     "£",        true,      "GBP"),
+    BTC("Bitcoin",                  "฿",        true,      "BTC"),
+    BCH("Bitcoin Cash",             "",         false,     "BCH"),
+    ETH("Ethereum",                 "Ξ",        true,      "ETH"),
+    LTC("Litecoin",                 "Ł",        true,      "LTC"),
+    XRP("Ripple",                   "",         false,      "XRP")
     ;
 
     private String friendlyName;
     private String symbol;
     private String code;
+    private boolean hasSymbol;
 
-    Currency(String friendlyName, String symbol, String code) {
+    Currency(String friendlyName, String symbol, boolean hasSymbol, String code) {
         this.friendlyName = friendlyName;
         this.symbol = symbol;
         this.code = code;
+        this.hasSymbol = hasSymbol;
     }
 
     public String getFriendlyName() { return friendlyName; }
     public String getSymbol() { return symbol; }
     public String getCode() { return code; }
-
-
+    public boolean hasSymbol() { return hasSymbol; }
 
     public static CharSequence[] getEntries() {
         CharSequence[] array = new CharSequence[values().length];
