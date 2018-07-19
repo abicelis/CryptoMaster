fc8575ec523467af893fb5607274235d2d370dc7
abicelis <abicelis@gmail.com>
2018-07-19 00:07:31 -0400

    Quick Room database fix

--- app/src/main/java/ve/com/abicelis/cryptomaster/application/Constants.java
+++ app/src/main/java/ve/com/abicelis/cryptomaster/application/Constants.java
@@ -53,6 +53,7 @@ public class Constants {
 
     /* ROOM DATABASE */
     public static final String                  ROOM_DATABASE_NAME =                                "crypto_master.db";
+    public static final int                     ROOM_DATABASE_VERSION =                             2;
 
 
     /* COINMARKETCAP API */
--- app/src/main/java/ve/com/abicelis/cryptomaster/data/local/AppDatabase.java
+++ app/src/main/java/ve/com/abicelis/cryptomaster/data/local/AppDatabase.java
@@ -3,6 +3,7 @@ package ve.com.abicelis.cryptomaster.data.local;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.RoomDatabase;
 
+import ve.com.abicelis.cryptomaster.application.Constants;
 import ve.com.abicelis.cryptomaster.data.model.Coin;
 import ve.com.abicelis.cryptomaster.data.model.FavoriteCoin;
 
@@ -10,7 +11,7 @@ import ve.com.abicelis.cryptomaster.data.model.FavoriteCoin;
  * Created by abicelis on 23/6/2018.
  */
 
-@Database(entities = {Coin.class, FavoriteCoin.class}, version = 1)
+@Database(entities = {Coin.class, FavoriteCoin.class}, version = Constants.ROOM_DATABASE_VERSION)
 //@TypeConverters({CalendarConverter.class})
 public abstract class AppDatabase extends RoomDatabase {
     public abstract CoinDao coinDao();
