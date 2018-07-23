76a1f8deca7a0a4a6bc602ce9d6dd12ad2f63100
abicelis <abicelis@gmail.com>
2018-07-23 18:05:33 -0400

    Added friendly names

--- app/src/main/java/ve/com/abicelis/cryptomaster/data/model/ChartTimeSpan.java
+++ app/src/main/java/ve/com/abicelis/cryptomaster/data/model/ChartTimeSpan.java
@@ -1,8 +1,31 @@
 package ve.com.abicelis.cryptomaster.data.model;
 
+import android.content.Context;
+import android.support.annotation.StringRes;
+
+import ve.com.abicelis.cryptomaster.R;
+
 /**
  * Created by abicelis on 3/7/2018.
  */
 public enum ChartTimeSpan {
-    _24H, _7D, _1M, _3M, _1Y, ALL
+    _24H(R.string.chart_time_span_24h),
+    _7D(R.string.chart_time_span_7d),
+    _1M(R.string.chart_time_span_1m),
+    _3M(R.string.chart_time_span_3m),
+    _1Y(R.string.chart_time_span_1y),
+    ALL(R.string.chart_time_span_all);
+
+
+    private @StringRes int mFriendlyNameRes;
+
+    ChartTimeSpan(int friendlyNameRes) {
+        mFriendlyNameRes = friendlyNameRes;
+    }
+
+    public String getFriendlyName(Context context) {
+        return context.getString(mFriendlyNameRes);
+    }
+
+    public @StringRes int getFriendlyNameRes() { return mFriendlyNameRes; }
 }
