3f3dd6ed67358c6fed1dfff0ee02a1d419fa4a35
abicelis <abicelis@gmail.com>
2018-07-27 17:18:38 -0400

    Tiny formatting fix

--- app/src/main/java/ve/com/abicelis/cryptomaster/util/StringUtil.java
+++ app/src/main/java/ve/com/abicelis/cryptomaster/util/StringUtil.java
@@ -9,7 +9,7 @@ import java.util.Locale;
 public class StringUtil {
 
     public static String withSuffix(double count) {
-        if (count < 1000) return "" + count;
+        if (count < 1000) return String.format(Locale.getDefault(), "%.1f", count);
         int exp = (int) (Math.log(count) / Math.log(1000));
         return String.format(Locale.getDefault(), "%.1f%c",
                 count / Math.pow(1000, exp),
