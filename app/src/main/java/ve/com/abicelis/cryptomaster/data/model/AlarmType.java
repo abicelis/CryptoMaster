02ca63a73377f208fe52bbf253efa2c3ed0cc9cd
abicelis <abicelis@gmail.com>
2018-09-22 09:34:28 -0400

    Bugfix!

--- app/src/main/java/ve/com/abicelis/cryptomaster/data/model/AlarmType.java
+++ app/src/main/java/ve/com/abicelis/cryptomaster/data/model/AlarmType.java
@@ -9,17 +9,19 @@ import ve.com.abicelis.cryptomaster.R;
  * Created by abicelis on 11/9/2018.
  */
 public enum AlarmType {
-    ABOVE(R.string.alarm_type_above, R.string.alarm_type_above_description),
-    BELOW(R.string.alarm_type_above, R.string.alarm_type_above_description);
+    ABOVE(R.string.alarm_type_above, R.string.alarm_type_above_description, R.string.alarm_type_above_description_2),
+    BELOW(R.string.alarm_type_below, R.string.alarm_type_below_description, R.string.alarm_type_below_description_2);
 
 
     private @StringRes int friendlyName;
     private @StringRes int description;
+    private @StringRes int description2;
 
 
-    AlarmType(@StringRes int friendlyName, @StringRes int description) {
+    AlarmType(@StringRes int friendlyName, @StringRes int description, @StringRes int description2) {
         this.friendlyName = friendlyName;
         this.description = description;
+        this.description2 = description2;
     }
 
 
@@ -34,8 +36,14 @@ public enum AlarmType {
     public @StringRes int getDescriptionRes() {
         return description;
     }
+    public @StringRes int getDescription2Res() {
+        return description2;
+    }
 
     public String getDescription(Context context) {
         return context.getString(description);
+    }
+    public String getDescription2(Context context) {
+        return context.getString(description2);
     }
 }
