f06b125ced72384a351ba7ee3bd9a4159de04ec2
abicelis <abicelis@gmail.com>
2018-10-01 19:09:21 -0400

    Bugfix

--- app/src/main/java/ve/com/abicelis/cryptomaster/MainActivity.kt
+++ app/src/main/java/ve/com/abicelis/cryptomaster/MainActivity.kt
@@ -23,7 +23,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
+        //menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
 
@@ -31,9 +31,10 @@ class MainActivity : AppCompatActivity() {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
+        //return when (item.itemId) {
+            //R.id.action_settings -> true
+            //else -> super.onOptionsItemSelected(item)
+        //}
+        return true
     }
 }
